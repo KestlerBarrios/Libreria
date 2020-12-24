2161b08d098e140f63b47bcda01c09cab2b0c219
Kestler Barrios <luiseduardo3213@gmail.com>
2020-12-24 01:29:37 -0600

    Agregar EntityFramework

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -9,9 +9,9 @@ namespace Libreria.Controllers
         public IActionResult Index()
         {
             var productos = new List<Producto>(){
-                new Producto{nombreProducto= "Crayones"},
-                new Producto{nombreProducto= "Marcadores"},
-                new Producto{nombreProducto= "Resaltadores"}
+                new Producto{nombreProducto= "Crayones", marca= "Tucan", precio= 12.4},
+                new Producto{nombreProducto= "Marcadores", marca= "Mis Pasitos", precio= 14.25},
+                new Producto{nombreProducto= "Resaltadores", marca= "Facela", precio= 18.265}
             };
 
             return View(productos);
