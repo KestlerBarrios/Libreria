e42c1dc42dd67e06bbd7183f69be17d6f9d29d61
Kestler Barrios <luiseduardo3213@gmail.com>
2020-12-30 00:10:15 -0600

    Lista de productos

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -17,13 +17,23 @@ namespace Libreria.Controllers
         public IActionResult Index(Producto producto)
         {
             producto.productoId = Guid.NewGuid().ToString();
-            _context.Productos.Add(producto); 
+            _context.Productos.Add(producto);
             _context.SaveChanges();
             return View();
         }
+        public IActionResult ProductoS(Producto producto)
+        {
+            return View(_context.Productos.ToList());
+        }
 
-        
-
+        [Route("ProductoController/ProductoSee/{productoId?}")]
+        public IActionResult ProductoSee(string productoId)
+        {
+            var Product = from produ in _context.Productos
+                          where produ.productoId == productoId
+                          select produ;
+            return View(Product.SingleOrDefault());
+        }
         public ProductoController(LibreriaContext context)
         {
             _context = context;
